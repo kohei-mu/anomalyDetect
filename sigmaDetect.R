78166d1a06b40044a7b849b4d5222d2dc3cc86bb
kohei-mu <kendo7bharat@gmail.com>
2017-03-08 00:25:17 +0900

    modified

--- sigmaDetect.R
+++ sigmaDetect.R
@@ -1,7 +1,7 @@
-x <- read.csv("C:/Users/kohei-mu/Downloads/indices_I101_1d_2016.csv", header = F)[,2]
+x <- read.csv("indices_I101_1d_2016.csv", header = F)[,2]
 
-sigmaCalculate <- function(sep, n, input) {
-  mat <- matrix(input, nrow=sep)
+sigmaCalculate <- function(term, n, input) {
+  mat <- matrix(input, nrow=term)
   ret <- NULL
   for(i in 1:length(mat[,1])){
     row <- mat[i,]
@@ -12,20 +12,39 @@ sigmaCalculate <- function(sep, n, input) {
     tmp <- c(range_r, range_l)
     ret <- rbind(ret,tmp)
   }
-  ranges <- data.frame(ret[,1],ret[,2])
+  sigma_ranges <- data.frame(right=ret[,1],left=ret[,2])
   
-  index <- 1
+  term_counter <- 1
   ret_final <- NULL
   for(v in input){
-    a_row <- c(v, ranges[index,])
-    if(index == sep){
-      index <- 1
+    a_row <- data.frame(value=v, sigma_ranges[term_counter,])
+    if(term_counter == term){
+      term_counter <- 1
     }else{
-      index <- index + 1
+      term_counter <- term_counter + 1
     }
     ret_final <- rbind(ret_final, a_row)
   }
+  
+  class <- NULL
+  for(i in 1:nrow(ret_final)){
+    row <- ret_final[i,]
+    if(row[,1] > row[,2] || row[,1] < row[,3]){
+      class_ <- 2
+    }else{
+      class_ <- 1
+    }
+    class <- c(class, class_)
+  }
+  result <- data.frame(ret_final, class)
 
-  return(ret_final)
+  return(result)
 }
 
+ret <- sigmaCalculate(49,1,x)
+plot(ret$value,type="l",ylab = "value")
+points(which(ret$class==2), ret$value[which(ret$class==2)],col=2)
+par(new=T)
+plot(ret$right,type="l", col=4,ylim=c(min(ret$value), max(ret$value)),ylab = "")
+par(new=T)
+plot(ret$left,type="l", col=4,ylim=c(min(ret$value), max(ret$value)), ylab = "")
