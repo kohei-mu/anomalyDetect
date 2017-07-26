a8f862cfb83d3643ddc7c36af996e3996cf6606f
kohei-mu <kendo7bharat@gmail.com>
2017-07-26 23:50:46 +0900

    modified

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,36 @@
 # -*- coding: utf-8 -*-
+#/usr/bin/env python
+
 """
 Created on Fri Jul 21 16:11:58 2017
 
 @author: muramatsu
 """
 
+import argparse
 import numpy as np
 import pandas as pd
 from datetime import datetime
+
+
+def parse_command_line():
+	parser = argparse.ArgumentParser(description="help messages")
+	group1 = parser.add_argument_group('Main options')
+	group2 = parser.add_argument_group('additional options')
+
+	group1.add_argument("-freq", dest="pvt_src", help="pivot-source phrase-table")
+	group1.add_argument("-anFreq", dest="pvt_tgt", help="pivot-target phrase-table")
+	group1.add_argument("-periods", dest="output", default="tmp_phrase-table", type=str, help="")
+	group1.add_argument("-anomalyNum", dest="merged_output", default="merged_phrase-table", type=str, help="")
+	group1.add_argument("-anomalyDirection", dest="norm_output", default="normalized_phrase-table", type=str, help="")
+	group1.add_argument("-resamleFreq", dest="lex_output", default="final_phrase-table", type=str, help="")
+	group1.add_argument("-tp", dest="tp", choices=[0, 1, 2, 3, 4, 5, 6], default=0, type=int, help="")
+
+	group2.add_argument("-resampleHow", dest="disfile", type=str, help=")")
+	group2.add_argument("-resampleHow", dest="tpnum", default=20, type=int, help="")
+	return parser.parse_args()
+
+
 
 def plot_func(df):
     df.plot()
@@ -23,8 +46,8 @@ def make_data(freq,periods):
     df=pd.DataFrame({"val":value},index=idx)
     return df
     
-df= make_data("5T",288)
-plot_func(df)
+#df= make_data("5T",288)
+#plot_func(df)
 
 def make_anomaly(df, periods, num, direction):
     start = np.random.randint(periods)
@@ -38,8 +61,8 @@ def make_anomaly(df, periods, num, direction):
         else:
             df["val"].iloc[start:end] = float(df.min()) / 10
     return df
-df = make_anomaly(df, 288, 20, "neg")
-plot_func(df)
+#df = make_anomaly(df, 288, 20, "neg")
+#plot_func(df)
     
 def resample_time(df, freq, how="mean"):
     if how == "last":
@@ -50,9 +73,10 @@ def resample_time(df, freq, how="mean"):
         df = df.resample(freq).mean()
         
     return df
-#df=resample_time(df,"5T","mean")
+#df=resample_time(df,"10T","mean")
+#plot_func(df)
 
-def missing_value(df, how):
+def missing_value(df, how="interpolate"):
     #indexer=np.random.randint(4,size=30)==1
     #df.loc[indexer]=np.nan
     if how == "interpolate":
@@ -67,14 +91,5 @@ def missing_value(df, how):
 #df = missing_value(df,"interpolate")
 
 
-start = np.random.randint(30)
-end = start + 10
-
-df.loc[start:end]=0
-
-if direction == "pos":
-    
-elif direction == "neg":
-        
 
 
