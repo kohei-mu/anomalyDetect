45607227023caf11635334395ba003a824fd9ff9
kohei-mu <kendo7bharat@gmail.com>
2018-11-13 15:43:08 +0900

    modified

--- changeFinderParamEstimate.py
+++ changeFinderParamEstimate.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Mar  6 09:09:50 2017
-
-@author: muramatsu
-"""
-
-# -*- coding: utf-
-# noinspection PyUnresolvedReferences
 import matplotlib.pyplot as plt
 import changefinder
 import numpy as np
@@ -17,53 +9,47 @@ from skopt.plots import plot_convergence
 import seaborn as sns
 import math
 
-#input data
-df = pd.read_csv("indices_I101.csv",header=None)[1]
-tmp =  np.array(df)
+train_data = np.array(pd.read_csv("toy.csv")["val0"])
 
-pcf = stt.pacf(tmp)
-# noinspection PyUnresolvedReferences
+pcf = stt.pacf(train_data)
+plt.bar(range(len(pcf)), pcf, width = 0.3)
+plt.show()
 lag = np.argsort(np.absolute(pcf))[::-1][1]
 
 #function to calculate change score
-def scoreCalculate(r,smooth):
+def scoreCalculate(r, smooth):
     cf = changefinder.ChangeFinder(r=r, order=lag, smooth=smooth)
     ret = []
-    for i in tmp:
+    for i in train_data:
         score = math.exp(cf.update(i))
         ret.append(score)
     return ret
     
-    
 #function to optimize
 def func(params):
-    r,smooth = params
+    r, smooth = params
     return np.var(scoreCalculate(r, smooth))
 
 #parameter bounds
-dimensions  = [(0.1, 0.5),   
-          (3,100),
+dimensions  = [(0.01, 0.1),
+            (3,50),
 ]     
 
 #minimize variance of change score
-res_gp = gp_minimize(func, dimensions, n_calls=10, random_state=0,n_random_starts=10)
+res_gp = gp_minimize(func, dimensions, n_calls=200, random_state=0)
 print(res_gp.x)
 
 #change score with best param
-score = scoreCalculate(res_gp.x[0],res_gp.x[1])
+score = scoreCalculate(res_gp.x[0], res_gp.x[1])
 
 #plot result
-fig = plt.figure(figsize=(13, 7))
+fig = plt.figure(figsize=(10, 7))
 ax = fig.add_subplot(111)
-ax.plot(score)
+ax.plot(score, label="anomaly score")
 ax2 = ax.twinx()
-ax2.plot(tmp,'r')
+ax2.plot(train_data, 'r', label="original data")
+plt.legend(loc='best', fontsize=18)
+plt.title("anomaly detection result by change finder")
 plt.show()
 
-#convergence
-plot_convergence(res_gp)
-#density of change score
-sns.distplot(score, hist=False)
 
-#output change score as csv
-pd.DataFrame(score).to_csv("score.csv")
